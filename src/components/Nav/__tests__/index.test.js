996eb4c321a56e32bc7cfe95e059d4f8a100e755
woodb58 <woodb58@gmail.com>
2022-07-07 17:24:25 -0500

    state to render gallery

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,3 +1,4 @@
+// __tests__/Nav.test.js with hard coded categories
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
@@ -6,29 +7,31 @@ import Nav from '..';
 afterEach(cleanup);
 
 describe('Nav component', () => {
-    // baseline test
-    it('renders', () => {
-      render(<Nav />);
-    });
-  
-    // snapshot test
-    it('matches snapshot', () => {
-      const { asFragment } = render(<Nav />);
-      // assert value comparison
-    });
-  })
+  it('renders', () => {
+    render(<Nav />);
+  });
 
-  describe('emoji is visible', () => {
-    it('inserts emoji into the h2', () => {
-    const { getByLabelText } = render(<Nav />);
-  
-    expect(getByLabelText('camera')).toHaveTextContent('📸');
-    });
-  })
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Nav />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
 
-  describe('links are visible', () => {
-    it('inserts text into the links', () => {
-      // Arrange
-      // Assert
-    });
-  })
+describe('emoji is visible', () => {
+  it('inserts emoji into the h2', () => {
+  const { getByLabelText } = render(<Nav />);
+
+  expect(getByLabelText('camera')).toHaveTextContent('📸');
+  });
+})  
+
+describe('links are visible', () => {
+  it('inserts text into the links', () => {
+    const { getByTestId } = render(<Nav />);
+
+    expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+    expect(getByTestId('about')).toHaveTextContent('About me');
+  });
+
+})
