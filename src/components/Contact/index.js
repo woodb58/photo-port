ac74a9a34d7c85afbc5e28c283faf76dd994ce58
woodb58 <woodb58@gmail.com>
2022-07-10 17:17:15 -0500

    commit

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,39 @@
-import react from "react";
+import React, { useState } from "react";
 
 function ContactForm() {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const { name, email, message } = formState;
+
+  function handleChange(e) {
+    setFormState({ ...formState, name: e.target.value });
+  }
+
+  console.log(formState); 
+
   return (
     <section>
       <h1>Contact me</h1>
       <form id="contact-form">
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" />
+          <input
+            type="text"
+            defaultValue={name}
+            onChange={handleChange}
+            name="name"
+          />
         </div>
         <div>
           <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" />
+          <input type="email" defaultValue={email} name="email" />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" />
+          <textarea name="message" defaultValue={message} rows="5" />
         </div>
         <button type="submit">Submit</button>
       </form>
